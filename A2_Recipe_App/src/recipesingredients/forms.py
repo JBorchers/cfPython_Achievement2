67d3a0bcfcc1ada1cf2e9f51448163387eda9125
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-09-01 11:33:25 -0600

    organize file structure

--- A2_Recipe_App/src/recipesingredients/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Ingredient
-
-class RecipeIngredientForm(forms.Form):
-    ingredients = forms.ModelMultipleChoiceField(
-        queryset=Ingredient.objects.all(),
-        widget=forms.CheckboxSelectMultiple
-    )
