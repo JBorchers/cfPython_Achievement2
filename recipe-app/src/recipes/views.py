bf1946e256b095d03182d8980ec7ed82f9396b38
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-09-11 20:37:18 -0600

    update 2.4

--- recipe-app/src/recipes/views.py
+++ recipe-app/src/recipes/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
-
 # Create your views here.
+def recipes_home(request):
+    return render(request, "recipes/recipes_home.html")
