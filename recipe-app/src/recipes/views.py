ce28283b960b32629bf5524d1fb3b6cb64f75c46
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-09-18 22:16:38 -0600

    2.5 first commit
    first draft of homepage; simple recipes list displayed

--- recipe-app/src/recipes/views.py
+++ recipe-app/src/recipes/views.py
@@ -5,3 +5,7 @@ from .models import Recipe
 # Create your views here.
 def recipes_home(request):
     return render(request, "recipes/recipes_home.html")
+
+class RecipesListView(ListView):
+    model = Recipe
+    template_name = "recipes/recipes_list.html"
