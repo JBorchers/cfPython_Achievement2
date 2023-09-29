d6a2ae48093cbb88e59cab2ee26e078780c39e62
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-09-28 20:51:39 -0600

    add description field and calculate difficulty function to model

--- recipe-app/src/recipes/models.py
+++ recipe-app/src/recipes/models.py
@@ -4,10 +4,30 @@ from django.shortcuts import reverse
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     cooking_time = models.PositiveIntegerField()
-    difficulty = models.CharField(max_length=20)
+    description = models.TextField(default="N/A")
+    difficulty = models.CharField(max_length=20, default="N/A")
     ingredients = models.ManyToManyField('ingredients.Ingredient', through='recipesingredients.RecipeIngredient', related_name='recipes')
     pic = models.ImageField(upload_to='recipes', default='no_picture.jpg') # default stored in src/media
-    
+
+    def calc_difficulty(self):
+        num_ingredients = self.ingredients.count()
+
+        if self.cooking_time < 10 and num_ingredients < 4:
+            return "Easy"
+        elif self.cooking_time < 10 and num_ingredients >= 4:
+            return "Medium"
+        elif self.cooking_time >= 10 and num_ingredients < 4:
+            return "Intermediate"
+        else:
+            return "Hard"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.difficulty = self.calc_difficulty()
+        super().save(update_fields=["difficulty"])
+
+
+
     def __str__(self):
         return self.name
     
