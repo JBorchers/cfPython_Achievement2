6731b5d2a4ac72c5f30d3bf72b9f87dcc4beaa33
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-10-08 14:39:39 -0600

    add search function to recipes_list

--- recipe-app/src/ingredients/models.py
+++ recipe-app/src/ingredients/models.py
@@ -6,3 +6,19 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        "recipes.Recipe", on_delete=models.CASCADE, related_name="ingredients_used"
+    )
+    ingredient = models.ForeignKey(
+        "ingredients.Ingredient",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="recipes_used",
+    )
+
+    def __str__(self):
+        return f"{self.ingredient} - {self.recipe}"
